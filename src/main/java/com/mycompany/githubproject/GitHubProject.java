56a6cddeea327412cbcfbc36dbd1e09f7bf4d802
RC_Student_lab <RC_Student_lab@10.0.0.159>
2024-09-09 08:23:31 +0000

    first commit- a program to add two numbers

--- src/main/java/com/mycompany/githubproject/GitHubProject.java
+++ src/main/java/com/mycompany/githubproject/GitHubProject.java
@@ -12,5 +12,9 @@ public class GitHubProject {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int a = 4;
+        int b = 10;
+        int c = a+b;
+        System.out.println("sum + " + c);
     }
 }
